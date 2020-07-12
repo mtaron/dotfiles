33d64c8cdd1d0f571f3e4975770a4bb7b276eb4b
Michael Taron <michael.taron@gmail.com>
2020-07-11 22:43:22 -0700

    Try manual install

--- install.sh
+++ install.sh
@@ -2,12 +2,11 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 cp --remove-destination ${BASEDIR}/p10k.zsh ~/.p10k.zsh
 
 cp --remove-destination ${BASEDIR}/zshenv ~/.zshenv
 
-sed -i 's#robbyrussell#powerlevel10k/powerlevel10k#' ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 
 rm -rf ${BASEDIR}

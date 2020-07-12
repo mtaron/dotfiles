4980a6870e2a3d31f09ee645e3dea02fc8f81cca
Michael Taron <michael.taron@gmail.com>
2020-07-11 22:51:14 -0700

    Add zshrc back

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 cp --remove-destination ${BASEDIR}/p10k.zsh ~/.p10k.zsh
 
 cp --remove-destination ${BASEDIR}/zshenv ~/.zshenv
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+cp --remove-destination ${BASEDIR}/zshrc ~/.zshrc
 
 rm -rf ${BASEDIR}

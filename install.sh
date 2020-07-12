562da249479f4471f60f6d739676ca564c32a74f
Michael Taron <michael.taron@gmail.com>
2020-07-11 22:29:22 -0700

    Try installing powerlevel

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-
-cp --remove-destination ${BASEDIR}/zshrc ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 cp --remove-destination ${BASEDIR}/p10k.zsh ~/.p10k.zsh
 
 cp --remove-destination ${BASEDIR}/zshenv ~/.zshenv
+
+sed -i 's#robbyrussell#powerlevel10k/powerlevel10k#' ~/.zshrc
 
 rm -rf ${BASEDIR}

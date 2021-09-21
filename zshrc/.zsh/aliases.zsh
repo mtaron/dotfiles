1fc3a7090d70fa7a096b623bc79297c791e4b4fb
Michael Taron <mtaron@docugami.com>
2021-09-21 10:42:32 -0700

    Remove ls alias - don't work on mac

--- zshrc/.zsh/aliases.zsh
+++ zshrc/.zsh/aliases.zsh
@@ -1,10 +1,2 @@
 alias zshrc="source $HOME/.zshrc"
 alias up='sudo apt update && sudo apt -V --yes upgrade'
-
-# ls
-# -v: natural sort of version
-alias ls='ls                                  -v --classify --group-directories-first --color=auto'
-alias  l='ls -l              --human-readable -v --classify --group-directories-first --color=auto'
-alias ll='ls -l              --human-readable -v --classify --group-directories-first --color=auto'
-alias la='ls -l --almost-all --human-readable -v --classify --group-directories-first --color=auto'
-

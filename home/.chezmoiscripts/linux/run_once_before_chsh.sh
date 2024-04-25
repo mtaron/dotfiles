7b2da01f1430c27b2219e3570d81bc5c010528a0
Michael Taron <mtaron@docugami.com>
2024-04-25 09:45:33 -0700

    trying to get dotfile install to work in devcontainer

--- home/.chezmoiscripts/linux/run_once_before_chsh.sh
+++ home/.chezmoiscripts/linux/run_once_before_chsh.sh
@@ -5,5 +5,5 @@ set -e
 zsh_path="$(command -v zsh)"
 if [ "$SHELL" != "$zsh_path" ]; then
     echo "Changing default shell to zsh"
-    chsh --shell "$zsh_path"
+    sudo chsh --shell "$zsh_path"
 fi

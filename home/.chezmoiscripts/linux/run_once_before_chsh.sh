4bdc59885f60e378c310092232388da82ba051ee
Michael Taron <mtaron@docugami.com>
2024-04-25 09:22:54 -0700

    use ppa for alacritty and run chsh before

--- home/.chezmoiscripts/linux/run_once_before_chsh.sh
+++ home/.chezmoiscripts/linux/run_once_before_chsh.sh
@@ -3,7 +3,7 @@
 set -e
 
 zsh_path="$(command -v zsh)"
-if [ "$zsh_path" ] && [ "$SHELL" != "$zsh_path" ]; then
+if [ "$SHELL" != "$zsh_path" ]; then
     echo "Changing default shell to zsh"
-    sudo chsh --shell "$zsh_path"
+    chsh --shell "$zsh_path"
 fi

7fdef6736d4028934445899c6a352c23df5fc475
Michael Taron <michael@taron.dev>
2025-12-11 21:56:52 -0800

    nixos: add in atuin's flake

--- home/private_dot_config/nixos/flake.nix
+++ home/private_dot_config/nixos/flake.nix
@@ -3,12 +3,25 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
+  inputs.atuin = {
+    url = "github:atuinsh/atuin";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
   outputs =
-    inputs@{ self, nixpkgs, ... }:
+    inputs@{
+      self,
+      nixpkgs,
+      atuin,
+      ...
+    }:
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+        modules = [
+          ./configuration.nix
+          { nixpkgs.overlays = [ atuin.overlays.default ]; }
+        ];
       };
     };
 }
